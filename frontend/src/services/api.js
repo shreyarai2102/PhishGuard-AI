9f6a1eeecfc15869e999b7ac61ffd57f63742844
shreyarai21 <shreyarai2102@gmail.com>
2026-07-12 16:09:41 +0000

    c11

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://bug-free-lamp-pjp6994xpv49h9rjr-8080.app.github.dev/",
 });
 
 export default api;
